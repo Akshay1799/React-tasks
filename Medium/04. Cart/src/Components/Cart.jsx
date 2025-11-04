8827e86a0bdbca88b9ba22a04772e20edbf3ffec
Akshay Ladne <Kumawata283@gmail.com>
2025-11-04 18:41:14 +0530

    Build a cart item counter

--- Medium/04. Cart/src/Components/Cart.jsx
+++ Medium/04. Cart/src/Components/Cart.jsx
@@ -14,18 +14,15 @@ const Cart = () => {
         setCount(prev => prev + 1)
     }
     return (
-        <div>
+        <div className='flex flex-col gap-4'>
             <div className='relative flex justify-center mx-auto 0 gap-4 w-fit  px-4 py-1 border border-gray rounded-full'>
                 <p>Cart</p>
                 <p className='top-[-15px] -right-1 absolute bg-red-500 border border-red-500 rounded-full px-2'>{count}</p>
             </div>
             {products.length > 0 && (
-                <div>
+                <div className='flex justify-center '>
                     {products.map((item, index) => (
-                        <div
-                            key={index}
-                            className='w-fit py-6 px-6 flex flex-col gap-4 border border-gray-200 rounded-xl m-4 mx-auto'
-                        >
+                        <div key={index} className='w-fit py-6 px-6 flex flex-col gap-4 border border-gray-200 rounded-xl m-2'>
                             <p className='font-semibold'>Item Name: {item.name}</p>
                             <p className='font-semibold text-2xl'>Price: {item.price}</p>
                             <button onClick={handleCartClick} className='bg-slate-700 hover:bg-black hover:cursor-pointer px-4 py-2 border rounded-lg text-white font-semibold'>
