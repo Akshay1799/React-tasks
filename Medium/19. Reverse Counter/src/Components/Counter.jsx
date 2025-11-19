22a20899a6183c91afc0ac598049080493a7864b
Akshay Ladne <Kumawata283@gmail.com>
2025-11-19 12:34:56 +0530

    build a reverse counter

--- Medium/19. Reverse Counter/src/Components/Counter.jsx
+++ Medium/19. Reverse Counter/src/Components/Counter.jsx
@@ -37,8 +37,8 @@ const Counter = () => {
 
 return (
     <div>
-        <p>{counter}</p>
-        <button onClick={startCounter} className='bg-black border border-transparent rounded-lg px-4 py-2'>start counter</button>
+        <p className='text-2xl font-bold my-6'>{counter}</p>
+        <button onClick={startCounter} className='hover:cursor-pointer hover:scale-95 duration-500 text-white font-semibold bg-black border border-transparent rounded-lg px-4 py-2'>start counter</button>
     </div>
 )
 
